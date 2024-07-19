d3f97d5d23e4fae72c06f6f9a3b3e675c702d187
Emre İnan <galahadem@hotmail.com>
2024-07-19 19:37:52 +0300

    Part three

---
+++ A2-spring_boot_hibernate/01-cruddemo_student/src/test/java/com/hemre/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemre.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
